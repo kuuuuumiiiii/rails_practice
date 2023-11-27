ff9061a1256b2ee991c7264ed10438cb9a100d7b
kuuuuumiiiii <kuuuuumiiiii@users.noreply.github.com>
2023-11-27 15:06:25 +0900

    first commit rails-practice

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  get 'boards', to: 'boards#index'
+  get 'boards/new', to: 'boards#new'
+end
